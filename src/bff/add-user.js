24cc751242e2884c13503526b1984c431c8b6196
Евгения Подопригора <eepodoprigora@gmail.com>
2024-10-11 17:22:11 +0300

    create bff

---
+++ src/bff/add-user.js
@@ -0,0 +1,14 @@
+import { generateDate } from './generate-date';
+
+export const addUser = (login, password) => {
+	fetch('http://localhost:3005/users', {
+		method: 'POST',
+		headers: 'Content-type: application/json;charset=utf-8',
+		body: JSON.stringify({
+			login,
+			password,
+			registred_at: generateDate(),
+			role_id: 3,
+		}),
+	});
+};
